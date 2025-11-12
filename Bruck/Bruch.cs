17a50833a68b03dc03a2c2c1cef93e2e447cf2ac
lars-leuenberger <leuenla11@gmail.com>
2025-11-12 07:57:13 +0100

    Aufgabe Bruch

---
+++ Bruck/Bruch.cs
@@ -0,0 +1,158 @@
+﻿using System;
+
+public class Bruch
+{
+    public int Zaehler { get; private set; }
+    public int Nenner { get; private set; }
+
+    public Bruch()
+    {
+        Zaehler = 0;
+        Nenner = 1;
+    }
+
+    public Bruch(int zaehler, int nenner)
+    {
+        if (nenner == 0)
+            throw new ArgumentException("Nenner darf nicht 0 sein!");
+
+        Zaehler = zaehler;
+        Nenner = nenner;
+        Normiere();
+        Kuerze();
+    }
+
+    public double AsDouble()
+    {
+        return (double)Zaehler / Nenner;
+    }
+
+    public Bruch BildeKehrwert()
+    {
+        if (Zaehler == 0)
+            throw new DivideByZeroException("Kehrwert von 0 nicht definiert!");
+        return new Bruch(Nenner, Zaehler);
+    }
+
+    public static Bruch Multipliziere(Bruch b1, Bruch b2)
+    {
+        return new Bruch(b1.Zaehler * b2.Zaehler, b1.Nenner * b2.Nenner);
+    }
+
+    public Bruch Multipliziere(Bruch b)
+    {
+        return new Bruch(Zaehler * b.Zaehler, Nenner * b.Nenner);
+    }
+
+    public Bruch Multipliziere(int f)
+    {
+        return new Bruch(Zaehler * f, Nenner);
+    }
+
+    public static Bruch Dividiere(Bruch b1, Bruch b2)
+    {
+        if (b2.Zaehler == 0)
+            throw new DivideByZeroException("Division durch 0!");
+        return new Bruch(b1.Zaehler * b2.Nenner, b1.Nenner * b2.Zaehler);
+    }
+
+    public Bruch Dividiere(Bruch b)
+    {
+        return Dividiere(this, b);
+    }
+
+    public Bruch Dividiere(int f)
+    {
+        if (f == 0)
+            throw new DivideByZeroException("Division durch 0!");
+        return new Bruch(Zaehler, Nenner * f);
+    }
+
+    public static Bruch Addiere(Bruch b1, Bruch b2)
+    {
+        int nenner = b1.Nenner * b2.Nenner;
+        int zaehler = b1.Zaehler * b2.Nenner + b2.Zaehler * b1.Nenner;
+        return new Bruch(zaehler, nenner);
+    }
+
+    public Bruch Addiere(Bruch b)
+    {
+        return Addiere(this, b);
+    }
+
+    public Bruch Addiere(int a)
+    {
+        return new Bruch(Zaehler + a * Nenner, Nenner);
+    }
+
+    public static Bruch Subtrahiere(Bruch b1, Bruch b2)
+    {
+        int nenner = b1.Nenner * b2.Nenner;
+        int zaehler = b1.Zaehler * b2.Nenner - b2.Zaehler * b1.Nenner;
+        return new Bruch(zaehler, nenner);
+    }
+
+    public Bruch Subtrahiere(Bruch b)
+    {
+        return Subtrahiere(this, b);
+    }
+
+    public Bruch Subtrahiere(int a)
+    {
+        return new Bruch(Zaehler - a * Nenner, Nenner);
+    }
+
+    public Bruch Erweitere(int f)
+    {
+        return new Bruch(Zaehler * f, Nenner * f);
+    }
+
+    private void Kuerze()
+    {
+        int gcd = GGT(Math.Abs(Zaehler), Math.Abs(Nenner));
+        Zaehler /= gcd;
+        Nenner /= gcd;
+    }
+
+    private void Normiere()
+    {
+        if (Nenner < 0)
+        {
+            Nenner *= -1;
+            Zaehler *= -1;
+        }
+    }
+
+    private static int GGT(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public override string ToString()
+    {
+        return $"{Zaehler}/{Nenner}";
+    }
+
+    public static Bruch operator *(Bruch b1, Bruch b2) => Multipliziere(b1, b2);
+    public static Bruch operator /(Bruch b1, Bruch b2) => Dividiere(b1, b2);
+    public static bool operator ==(Bruch b1, Bruch b2) => b1.Equals(b2);
+    public static bool operator !=(Bruch b1, Bruch b2) => !b1.Equals(b2);
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Bruch b)
+            return Zaehler == b.Zaehler && Nenner == b.Nenner;
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Zaehler, Nenner);
+    }
+}
---
+++ BruckTests/BruchTests.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests
+{
+    [TestClass()]
+    public class BruchTests
+    {
+        [TestMethod()]
+        public void Konstruktor_KorrektNormiertUndGekuerzt()
+        {
+            Bruch b = new Bruch(-2, -4);
+            Assert.AreEqual(1, b.Zaehler);
+            Assert.AreEqual(2, b.Nenner);
+        }
+
+        [TestMethod()]
+        public void Konstruktor_NennerNull_WirftException()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new Bruch(1, 0));
+        }
+
+        [TestMethod()]
+        public void AsDouble_BerechnetKorrekt()
+        {
+            Bruch b = new Bruch(1, 2);
+            Assert.AreEqual(0.5, b.AsDouble(), 0.0001);
+        }
+
+        [TestMethod()]
+        public void BildeKehrwert_FunktioniertKorrekt()
+        {
+            Bruch b = new Bruch(2, 3);
+            Bruch kehrwert = b.BildeKehrwert();
+            Assert.AreEqual(3, kehrwert.Zaehler);
+            Assert.AreEqual(2, kehrwert.Nenner);
+        }
+
+        [TestMethod()]
+        public void BildeKehrwert_VonNull_WirftException()
+        {
+            Bruch b = new Bruch(0, 5);
+            Assert.ThrowsException<DivideByZeroException>(() => b.BildeKehrwert());
+        }
+
+        [TestMethod()]
+        public void Multipliziere_ZweiBrueche()
+        {
+            Bruch b1 = new Bruch(2, 3);
+            Bruch b2 = new Bruch(3, 4);
+            Bruch result = Bruch.Multipliziere(b1, b2);
+            Assert.AreEqual(1, result.Zaehler);
+            Assert.AreEqual(2, result.Nenner); // 6/12 -> 1/2
+        }
+
+        [TestMethod()]
+        public void Dividiere_ZweiBrueche()
+        {
+            Bruch b1 = new Bruch(1, 2);
+            Bruch b2 = new Bruch(3, 4);
+            Bruch result = Bruch.Dividiere(b1, b2);
+            Assert.AreEqual(2, result.Zaehler);
+            Assert.AreEqual(3, result.Nenner);
+        }
+
+        [TestMethod()]
+        public void Addiere_ZweiBrueche()
+        {
+            Bruch b1 = new Bruch(1, 2);
+            Bruch b2 = new Bruch(1, 3);
+            Bruch result = Bruch.Addiere(b1, b2);
+            Assert.AreEqual(5, result.Zaehler);
+            Assert.AreEqual(6, result.Nenner);
+        }
+
+        [TestMethod()]
+        public void Subtrahiere_ZweiBrueche()
+        {
+            Bruch b1 = new Bruch(3, 4);
+            Bruch b2 = new Bruch(1, 2);
+            Bruch result = Bruch.Subtrahiere(b1, b2);
+            Assert.AreEqual(1, result.Zaehler);
+            Assert.AreEqual(4, result.Nenner);
+        }
+
+        [TestMethod()]
+        public void Operatoren_MultiplikationDivision()
+        {
+            Bruch b1 = new Bruch(2, 3);
+            Bruch b2 = new Bruch(3, 4);
+            Assert.AreEqual(new Bruch(1, 2), b1 * b2);
+            Assert.AreEqual(new Bruch(8, 9), b1 / b2);
+        }
+
+        [TestMethod()]
+        public void Gleichheit_GekuerztVergleichen()
+        {
+            Bruch b1 = new Bruch(1, 2);
+            Bruch b2 = new Bruch(2, 4);
+            Assert.IsTrue(b1 == b2);
+            Assert.IsFalse(b1 != b2);
+        }
+
+        [TestMethod()]
+        public void ToString_FormatiertKorrekt()
+        {
+            Bruch b = new Bruch(2, 3);
+            Assert.AreEqual("2/3", b.ToString());
+        }
+    }
+}
